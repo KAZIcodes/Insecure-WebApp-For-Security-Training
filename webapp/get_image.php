6bdfdaba3a126d3777fdc9c896b8f232d5a46d4d
Amirali Kazerooni <amirkazi3344@gmail.com>
2023-11-24 10:45:49 +0330

    Delete webapp directory

--- webapp/get_image.php
+++
@@ -1,5 +0,0 @@
-<?php
-$imagePath = $_GET['imgsrc'];
-$imageData = file_get_contents($imagePath); 
-echo $imageData;
-?>
--- webapp/statics/footer.php
+++
@@ -1,8 +0,0 @@
-<footer>
-        <p>&copy; 2023 Amirali Kazerooni</p>
-    </footer>
-
-    <script src="app.js"></script>
-    </body>
-
-</html>
